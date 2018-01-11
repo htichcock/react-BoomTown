50849ef310128c0941742715b43394a99517ca82
Ian <ianhitchcock@outlook.com>
2018-01-11 15:20:56 -0800

    Use moment and crypto-js to complete CarHeader

--- client/src/containers/Items/Items.js
+++ client/src/containers/Items/Items.js
@@ -7,42 +7,42 @@ import {
   CardTitle,
   CardText
 } from "material-ui/Card";
+import Masonry from "react-masonry-component";
 import FlatButton from "material-ui/FlatButton";
 import PropTypes from "prop-types";
 
+import moment from "moment";
 const Items = ({ list }) => {
   return list.length ? (
-    <div>
-      {list.map(item => {
-        return (
-          <Card key={item.id}>
-            {item.borrower ? (
-              <CardMedia
-                overlay={
-                  <CardTitle title={`Lent to ${item.borrower.fullname}`} />
-                }
-              >
-                <img src={item.imageurl} alt={item.title} />
-              </CardMedia>
-            ) : (
-              <CardMedia>
-                <img src={item.imageurl} alt={item.title} />
-              </CardMedia>
-            )}
-            <CardHeader
-              title={item.itemowner.fullname}
-              subtitle="Subtitle" //todo: Date passed since post moment.js
-              avatar="https://www.gravatar.com/avatar/3391cb8fba76063554633df6f0cbb0d9.jpg" //todo: gravatar
-            />
-            <CardTitle title={item.title} subtitle={item.tags.join(", ")} />
-            <CardText>{item.description}</CardText>
-            <CardActions>
-              {!item.borrower && <FlatButton label="borrow" />}
-            </CardActions>
-          </Card>
-        );
-      })}
-    </div>
+    list.map(item => {
+      return (
+        <Card key={item.id}>
+          {item.borrower ? (
+            <CardMedia
+              overlay={
+                <CardTitle title={`Lent to ${item.borrower.fullname}`} />
+              }
+            >
+              <img src={item.imageurl} alt={item.title} />
+            </CardMedia>
+          ) : (
+            <CardMedia>
+              <img src={item.imageurl} alt={item.title} />
+            </CardMedia>
+          )}
+          <CardHeader
+            title={item.itemowner.fullname}
+            subtitle={moment(item.created).fromNow()} //todo: Date passed since post moment.js
+            avatar={item.itemowner.gravatarurl}
+          />
+          <CardTitle title={item.title} subtitle={item.tags.join(", ")} />
+          <CardText>{item.description}</CardText>
+          <CardActions>
+            {!item.borrower && <FlatButton label="borrow" />}
+          </CardActions>
+        </Card>
+      );
+    })
   ) : (
     //insert loader....
     <div>Loading</div>
--- client/src/containers/Items/ItemsContainer.js
+++ client/src/containers/Items/ItemsContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Items from "./Items";
+import MD5 from "crypto-js/md5";
 
 const ITEMS_URL = "http://localhost:4000/items";
 const USERS_URL = "http://localhost:4000/users";
@@ -21,11 +22,20 @@ export default class ItemsContainer extends Component {
       //todo: merge two lists into single list.
       //attach the new list to state and pass into items as props
       //items should render the list
+
       let itemsArray = response[0];
       let usersArray = response[1];
+      console.log(usersArray);
+      console.log(MD5(usersArray[0].email).toString());
       let userTable = {};
-      usersArray.forEach(user => {
+      usersArray = usersArray.map(user => {
+        //generate userTable
         userTable[user.id] = user;
+        //add gravatar url
+        user.gravatarurl = `//www.gravatar.com/avatar/${MD5(
+          user.email
+        ).toString()}.jpg`;
+        return user;
       });
       newStateItems = itemsArray.map(item => {
         if (item.itemowner) {
